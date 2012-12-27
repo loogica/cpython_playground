9fdb35c920a67bd435836ec4f751bdd2c0913db1
Felipe cruz <felipecruz@loogica.net>
2012-12-26 22:28:01 -0200

    fix setup extension modules configuration

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup, Extension
 object_protocol = Extension('object_protocol_playground',
                             sources=['src/object_protocol.c'])
 
-object_protocol = Extension('number_protocol_playground',
+number_protocol = Extension('number_protocol_playground',
                             sources=['src/number_protocol.c'])
 
 setup(name='protocol_playground',
       version='1.0',
       description='For Educational Purposes',
-      ext_modules=[object_protocol])
+      ext_modules=[number_protocol, object_protocol])
